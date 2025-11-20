506bb005bff2bf4347b1252464af93a18fe7599f
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-11-20 13:59:34 +0600

    Add files via upload

---
+++ Arnab/mishon.js
@@ -0,0 +1,16 @@
+var a = document.querySelector("#bulb")
+var b = document.querySelector("button")
+var flag=0
+b.addEventListener("click", function(){
+    if (flag==0)
+    {
+       a.style.backgroundColor = "yellow"
+       flag=1 
+    }
+    else{
+a.style.backgroundColor = "white"
+flag=0
+    }
+    
+})
+
